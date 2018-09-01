5f82a0f221f431cf8d181a7fb5b783989678ccba
shimon moyal <shimon83@gmail.com>
2018-09-02 01:54:58 +0300

    Add files via upload

---
+++ mysite/crypto/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+def home(request):
+    import requests
+    import json
+    #coin prices from cryptocompare api
+    price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,LTC,XLM,ADA,USDT,MIOTA,TRX&tsyms=USD")
+    price = json.loads(price_request.content)
+
+    #crypto news from cryptocompare api
+    api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api = json.loads(api_request.content)
+    return render(request, 'home.html', {'api':api,'price':price})
+# Create your views here.
+
+def prices(request):
+    if request.method == 'POST':
+        import requests
+        import json
+        quote = request.POST['quote']
+        quote = quote.upper()
+        crypto_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + quote + "&tsyms=USD")
+        crypto = json.loads(crypto_request.content)
+        return render(request, 'prices.html', {'quote':quote, 'crypto': crypto})
+    else:
+        notfound = "There is no such cryptocurrency"
+        return render(request, 'prices.html', {'notfound': notfound})
